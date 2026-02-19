68667450e4acb83edcac514e659228ef86aed705
ilhamsheva <ilhamshv@gmail.com>
2026-02-20 05:25:14 +0700

    feat: Add database migrate using PostgreSQL

---
+++ migrations/1771539101495_create-table-album.js
@@ -0,0 +1,36 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.createTable('album', {
+        id: {
+            type: 'VARCHAR(75)',
+            unique: true,
+            primaryKey: true
+        },
+        name: {
+            type: 'VARCHAR(255)',
+            notNull: true
+        },
+        year: {
+            type: 'INTEGER',
+            notNull: true
+        }
+    });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.dropTable('album');
+};
